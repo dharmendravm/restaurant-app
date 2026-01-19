6fe9a79f7d637a050ec70cfd5e182421e30d2c61
Dharmendra Verma <its.dharmendra.v@gmail.com>
2026-01-19 11:39:08 +0530

    server.js

---
+++ server/server.js
@@ -0,0 +1,9 @@
+import http from "http";
+import app from "./app.js";
+const port = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server is Running on Port: ${port}"`);
+});
